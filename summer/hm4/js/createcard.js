4bac155d498fe19d64e5cdae4222f19abcfbfa46
Shadoll <chenzijian13@gmail.com>
2015-07-15 19:29:12 +0800

    no message

--- summer/hm4/js/createcard.js
+++ summer/hm4/js/createcard.js
@@ -3,10 +3,29 @@ function addcard(item, listNum) {//card 是从json中获取的卡片对象，lis
     card.attr('class', 'card-container');
     var img = $('<img/>');
     img.attr('alt', item.name);
-    //img.attr('src', "images/" + item.fileName);
     img.attr('src', "images/loading.gif");
-    img.error(function () {$(this).attr('src', 'wrong.jpg')});
+    img.error(handleError.bind(img));
     img.load(function () {$(this).attr('src', "images/" + item.fileName);});
     card.append(img);
+    console.log('this 1111');
     $($('.cards-list')[listNum]).append(card);
+}
+
+function preLoad(imgBuffer, item) {
+    if (imgBuffer instanceof Array) {
+        var card = $('<li/>');
+        card.attr('class', 'card-container');
+        var img = $('<img/>');
+        img.attr('alt', item.name);
+        img.attr('src', "images/loading.gif");
+        img.error(handleError);
+        img.load(function () {$(this).attr('src', "images/" + item.fileName);});
+        card.append(img);
+        imgBuffer.push(card);
+    }
+}
+
+function handleError() {
+     $(this).attr('src', 'wrong.jpg');
+     $(this).unbind();
 }
--- summer/hm4/js/main.js
+++ summer/hm4/js/main.js
@@ -1,10 +1,12 @@
 //全局变量
-var cardNum = 20;
-var cards;
-var currentShowCard = 1;
-var currentCommentPage = 0;
-var locationX, locationY;
+var cardNum = 12;//初始化页面载入的图片数
+var preloadNum = cardNum;//预载入图片的编号
+var currentCommentPage = 0;//查看大图评论时的当前评论页数
+var locationX, locationY;//用户地理位置，X：经度，Y:纬度
+var imgBuffer = [];//与载入图片缓冲区
+var timerID = [];
 
+//初始化页面
 function init() {
     $.ajax({
         type: 'GET',
@@ -12,12 +14,15 @@ function init() {
         dataType: 'json',
         success: function (data){
             for(var i = 0; i < cardNum; ++i) {
-                cards = data;
-                addcard(cards[i],i % 4);
-            }}
-    });
+                addcard(data[i],i % 4);
+            }
+            for (var i = 0; i < 8; ++i) {
+                preLoad(imgBuffer, data[preloadNum + i]);
+            }
+            preloadNum += 8;
+    }});
 }
-
+//点击小图查看大图
 function show () {
     var bigcard = $('#big-card');
     bigcard.fadeIn(500);
@@ -37,7 +42,6 @@ function show () {
                     var d = getDistance(locationX, locationY, data[i].location.x, data[i].location.y);
                     dis.text("这张图片距离您" + d + "km.");
                     comment.text(data[i].comment[0].words);
-                    //author.text(data[i].comment[0].name);
                     author.html(data[i].comment[0].name + " <span class=\"date\">" + data[i].comment[0].date + "</span");
                     page.text("第1页,共" + data[i].comment.length +"页");
                     currentCommentPage = 0;
@@ -51,7 +55,7 @@ function show () {
     $('#comment-info').append(comment);
     $('#comment-info').append($('<hr/>'));
 }
-
+//隐藏大图
 function hide () {
     var bigcard = $('#big-card');
     $('#big-card-img').empty();
@@ -59,7 +63,7 @@ function hide () {
     $('#comment-info').empty();
     bigcard.fadeOut(500);
 }
-
+//查看下一页评论
 function next (e) {
     e.preventDefault();
     var str = $('#big-card-img img').attr('src').substring(7);
@@ -76,7 +80,6 @@ function next (e) {
                     if (currentCommentPage < data[i].comment.length - 1) {
                         ++currentCommentPage;
                         comment.text(data[i].comment[currentCommentPage].words);
-                        //author.text(data[i].comment[currentCommentPage].name);
                         author.html(data[i].comment[currentCommentPage].name + " <span class=\"date\">" + data[i].comment[currentCommentPage].date + "</span");
                         page.text("第" + (currentCommentPage + 1) + "页,共" + data[i].comment.length +"页");
                         break;
@@ -86,7 +89,7 @@ function next (e) {
         }
     });
 }
-
+//查看上一页评论
 function back (e) {
     e.preventDefault();
     var str = $('#big-card-img img').attr('src').substring(7);
@@ -103,7 +106,6 @@ function back (e) {
                     if (currentCommentPage > 0) {
                         --currentCommentPage;
                         comment.text(data[i].comment[currentCommentPage].words);
-                        //author.text(data[i].comment[currentCommentPage].name);
                         author.html(data[i].comment[currentCommentPage].name + " <span class=\"date\">" + data[i].comment[currentCommentPage].date + "</span");
                         page.text("第" + (currentCommentPage + 1) + "页,共" + data[i].comment.length + "页");
                         break;
@@ -113,7 +115,7 @@ function back (e) {
         }
     });
 }
-
+//获得用户地理位置
 function getLocation () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -127,16 +129,16 @@ function showPosition (position) {
     locationY = position.coords.latitude;
     locationX = position.coords.longitude;
 }
-
+//计算两点间球面距离
 function getDistance(locationX, locationY, x, y){
-    var x1 = Math.cos(locationX) * Math.cos(locationY);
-    var x2 = Math.cos(y) * Math.cos(x);
-    var y1 = Math.cos(locationY) * Math.sin(locationX);
-    var y2 = Math.cos(y) * Math.sin(x);
-    var z1 = Math.sin(locationY);
-    var z2 = Math.sin(y);
+    var x1 = Math.cos(locationX / 180 * Math.PI) * Math.cos(locationY / 180 * Math.PI);
+    var x2 = Math.cos(y / 180 * Math.PI) * Math.cos(x / 180 * Math.PI);
+    var y1 = Math.cos(locationY / 180 * Math.PI) * Math.sin(locationX / 180 * Math.PI);
+    var y2 = Math.cos(y / 180 * Math.PI) * Math.sin(x / 180 * Math.PI);
+    var z1 = Math.sin(locationY / 180 * Math.PI);
+    var z2 = Math.sin(y / 180 * Math.PI);
     var a = Math.acos(x1*x2 + y1*y2 + z1*z2);
-    var d = a / 180 * Math.PI * 6371;
+    var d = a * 6371;
     return d;
 }
 
@@ -156,7 +158,7 @@ function showError (error) {
             break;
     }
 }
-
+//载入页面时触发的事件
 window.onload = function () {
     init();
     $('.cards-table').on('click', 'img', show);
@@ -168,29 +170,37 @@ window.onload = function () {
     });
     getLocation();
 };
-
+//滚动条下滚时触发的事件
 window.onscroll = function () {
     var scrollTop = document.body.scrollTop;
     var scrollHeight = document.body.scrollHeight;
     var clientHeight = document.body.clientHeight;
     if (scrollTop >= scrollHeight - clientHeight - 3000) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function(event) {
-            if (xhr.readyState == 4) {
-                if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304 || xhr.status == 0) {
-                    //这里在实际中应该通过后台程序根据cardNum获取特定的数据，而不是全部获取，但是目前没学后台就先这样吧
-                    cards = eval('(' + xhr.responseText + ')');
+        $.ajax({
+            type: 'GET',
+            url: 'https://raw.githubusercontent.com/Shaddoll/Shaddoll.github.io/master/cards.json',
+            dataType: 'json',
+            success: function (data){
+                for (var i = 0; i < 8 && preloadNum + i < data.length && imgBuffer.length < 16; ++i) {
+                    preLoad(imgBuffer, data[preloadNum + i]);
                 }
-                else {
-                    console.log("Request was unsuccessful:" + xhr.status);
-                }
-            }
-        }
-        xhr.open('GET', 'https://raw.githubusercontent.com/Shaddoll/Shaddoll.github.io/master/cards.json', true);
-        xhr.send(null);
-
-
-        addcard(cards[cardNum], cardNum % 4);
-        ++cardNum;
+                preloadNum += i;
+        }});
+        timerID.push(setInterval(loadcard, 100));
     }
 };
+
+function loadcard() {
+    var scrollTop = document.body.scrollTop;
+    var scrollHeight = document.body.scrollHeight;
+    var clientHeight = document.body.clientHeight;
+    if (scrollTop >= scrollHeight - clientHeight - 1500 && imgBuffer.length > 0) {
+        $($('.cards-list')[cardNum % 4]).append(imgBuffer.shift());
+        ++cardNum;
+    }
+    else {
+        for (var i = 0; i < timerID.length; ++i) {
+            clearInterval(timerID[i]);
+        }
+    }
+}
